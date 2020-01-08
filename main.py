b7775288a4eb9bf90938ed19e602b18085c01ee7
Shenda Hong <hongshenda@pku.edu.cn>
2020-01-08 11:16:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ if __name__ == "__main__":
     before running preprocess_physionet, 
     please first download the raw data from https://physionet.org/content/challenge-2017/1.0.0/, 
     and put it in data_path
+    Or you can download my preprocessed dataset challenge2017.pkl from https://drive.google.com/drive/folders/1AuPxvGoyUbKcVaFmeyt3xsqj6ucWZezf
     """
 
     # prepare data
     data_path = '../../data/challenge2017/'
-    # preprocess_physionet(data_path) # uncomment if you don't have the raw data
+    # preprocess_physionet(data_path) # uncomment if you don't have the raw data, omit this if you have challenge2017.pkl
     # make_data_physionet(data_path) # uncomment if you don't have the preprocessed data
     # make_knowledge_physionet(data_path) # uncomment if you don't have the preprocessed knowledge
     
